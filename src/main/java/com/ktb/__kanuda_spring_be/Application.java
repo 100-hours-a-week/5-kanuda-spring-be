7cd4a2a3c333ce88fa79f375dc9005180fd1875d
DohunHyun <dohun.hyun@gmail.com>
2024-06-13 17:06:32 +0900

    add: init

---
+++ src/main/java/com/ktb/__kanuda_spring_be/Application.java
@@ -0,0 +1,13 @@
+package com.ktb.__kanuda_spring_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb/__kanuda_spring_be/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.__kanuda_spring_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
