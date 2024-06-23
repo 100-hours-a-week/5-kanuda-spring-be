03e9d4c63328c9e6f79749e35c2e1490b0165ae3
DohunHyun <dohun.hyun@gmail.com>
2024-06-23 11:06:12 +0900

    feat: 어노테이션 추가

--- src/main/java/com/ktb/community/config/SecurityConfig.java
+++ src/main/java/com/ktb/community/config/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.ktb.community.config;
 import com.ktb.community.auth.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
